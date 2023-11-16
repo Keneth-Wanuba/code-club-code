6d328d35cc27d896f3c5337c8e991a87bd520a0f
viola <violatugumisirize990gmail.com>
2023-11-16 11:32:54 +0300

    changes

---
+++ admin_panel/adminView/viewAllProducts.php
@@ -0,0 +1,109 @@
+
+<div >
+  <h2>Product Items</h2>
+  <table class="table ">
+    <thead>
+      <tr>
+        <th class="text-center">Student_ID.</th>
+        <th class="text-center">Std Image</th>
+        <th class="text-center">Sdt Name</th>
+        <th class="text-center">Sdt Description</th>
+        <th class="text-center">School</th>
+        <th class="text-center">level</th>
+        <th class="text-center" colspan="2">Action</th>
+      </tr>
+    </thead>
+    <?php
+      include_once "../config/dbconnect.php";
+      $sql="SELECT * from product, category WHERE product.school_id=category.school_id";
+      $result=$conn-> query($sql);
+      $count=1;
+      if ($result-> num_rows > 0){
+        while ($row=$result-> fetch_assoc()) {
+    ?>
+    <tr>
+      <td><?=$count?></td>
+      <td><img height='100px' src='<?=$row["std_image"]?>'></td>
+      <td><?=$row["student_name"]?></td>
+      <td><?=$row["student_desc"]?></td>      
+      <td><?=$row["school_id"]?></td> 
+      <td><?=$row["level"]?></td>
+
+      <td><button class="btn btn-primary" style="height:40px" onclick="itemEditForm('<?=$row['student_id']?>')">Edit</button></td>
+      <td><button class="btn btn-danger" style="height:40px" onclick="itemDelete('<?=$row['student_id']?>')">Delete</button></td>
+      </tr>
+      <?php
+            $count=$count+1;
+          }
+        }
+      ?>
+  </table>
+
+  <!-- Trigger the modal with a button -->
+  <button type="button" class="btn btn-secondary " style="height:40px" data-toggle="modal" data-target="#myModal">
+    Add Student
+  </button>
+
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title">New Student</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        <div class="modal-body">
+          <form  enctype='multipart/form-data' onsubmit="addItems()" method="POST">
+            <div class="form-group">
+              <label for="name">Student Name:</label>
+              <input type="text" class="form-control" id="p_name" required>
+            </div>
+            <div class="form-group">
+              <label for="price">Level:</label>
+              <input type="number" class="form-control" id="p_price" required>
+            </div>
+            <div class="form-group">
+              <label for="qty">Description:</label>
+              <input type="text" class="form-control" id="p_desc" required>
+            </div>
+            
+            <div class="form-group">
+              <label>School:</label>
+              <select id="category" >
+                <option disabled selected>Select School</option>
+                <?php
+
+                  $sql="SELECT * from category";
+                  $result = $conn-> query($sql);
+
+                  if ($result-> num_rows > 0){
+                    while($row = $result-> fetch_assoc()){
+                      echo"<option value='".$row['school_id']."'>".$row['school_name'] ."</option>";
+                    }
+                  }
+                ?>
+              </select>
+            </div>
+            <div class="form-group">
+                <label for="file">Choose Image:</label>
+                <input type="file" class="form-control-file" id="file">
+            </div>
+            <div class="form-group">
+              <button type="submit" class="btn btn-secondary" id="upload" style="height:40px">Add Student</button>
+            </div>
+          </form>
+
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal" style="height:40px">Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+
+  
+</div>
+   
---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+require_once "admin_panel/config/dbconnect.php"
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Reports</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/font-awesome-4.7.0/css/font-awesome.css">
+</head>
+<body>
+
+    <header>
+        <div class="logos">
+            <img src="imgs/Logos/Code Academy logo-15.png" alt="Your Logo" width="100" height="100">
+            <h>WCIS</h>
+        </div>
+        <nav class="navigation">
+           
+                <li id="current" class="menuitem"><a href="index.html">Home</a></li>
+                <li><a href="student_report.html">Generate Report</a></li>
+            
+        </nav>
+        
+</body>
+</html>
